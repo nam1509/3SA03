33964db8864bdaf50db34dd001f7a155128c83ee
nam1509 <mylovenam126@gmail.com>
2018-10-03 00:03:39 +0700

    แนวนอน

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard.js';
-export default class WordCard.js extends
+export default class WordCard extends
 Component {
 render() {
  return (
- <div>
+ <div >
     {   Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
 
  </div>
