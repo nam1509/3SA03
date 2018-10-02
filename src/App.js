0e469363362b63de8aed6bbd1fd001ec56cd31a7
nam1509 <mylovenam126@gmail.com>
2018-10-02 23:51:25 +0700

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
-import CharacterCard from './WordCard';
-const word = "Hello";
+import WordCard from './WordCard.js';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="card">
         <WordCard value="hello"/>
     </div>
     );
