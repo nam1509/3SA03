c21c5ad7c9b89bf147152742ffa1dfa6570d79f4
nam1509 <mylovenam126@gmail.com>
2018-10-03 00:11:23 +0700

    Write componentWillMount in WordCard

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard.js';
+import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+ let word = given_word.toUpperCase()
+ let chars = _.shuffle(Array.from(word))
+ return {
+ word,
+ chars,
+ attempt: 1,
+ guess: [],
+ completed: false
+ }
+}
 
 export default class WordCard extends Component {
     constructor(props){
@@ -8,12 +20,23 @@ export default class WordCard extends Component {
             attempt: '',
         }
     }
+    
+    componentWillMount(){
+        let data = prepareStateFromWord(this.props.value);
+        this.setState({
+            word : data.word,
+            chars: data.chars,
+            attempt: data.attempt,
+            guess:data.guess,
+            completed:data.completed,
+        })
+    }
 
 
     activationHandler = c => { 
         console.log(`${c} has been activated.`) 
     }
-
+    
     render() {
         return (
             <div >
