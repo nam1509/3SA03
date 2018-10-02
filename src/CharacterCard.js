7ac223ddfb1f271e819b508d3e8af7287130baec
nam1509 <mylovenam126@gmail.com>
2018-10-02 23:58:39 +0700

    เพิ่ม js

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-export default class CharacterCard extends Component {
+export default class CharacterCard.js extends Component {
     constructor(props){
     super(props)
     this.state = {
