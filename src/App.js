3ff21f9ba053bf4c8b17c39cae5a377f2d621441
nam1509 <mylovenam126@gmail.com>
2018-10-02 23:50:26 +0700

    change app.js add WordCard in render

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+import CharacterCard from './WordCard';
 const word = "Hello";
 
 class App extends Component {
   render() {
     return (
-      <div className="card">
-        {
-          Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-        }
-     </div>
+      <div>
+        <WordCard value="hello"/>
+    </div>
     );
   }
 }
