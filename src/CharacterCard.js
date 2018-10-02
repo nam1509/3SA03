13024a5e42efc1e484018367f8607494ce56847a
nam1509 <mylovenam126@gmail.com>
2018-10-03 00:06:37 +0700

    add activate in CharacterCard Give printf in console

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -15,6 +15,13 @@ export default class CharacterCard extends Component {
             this.setState({ active: true})
         }else this.setState({ active: false})
     }
+    activate = () => {
+        if(!this.state.active){
+        this.props.activationHandler(this.props.value)
+        this.setState({active: true})
+        }
+       }
+       
     render() {
         let className = `card ${this.state.active ? 'activeCard' : ''}`
         return (
