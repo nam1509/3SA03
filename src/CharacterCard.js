30f7fb1137b3279fc55385f9e61365f47c9b5d79
nam1509 <mylovenam126@gmail.com>
2018-10-03 00:01:27 +0700

    fix error in CharacterCard in line 15 Work

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-export default class CharacterCard.js extends Component {
-    constructor(props){
-    super(props)
-    this.state = {
-    active: false,
+export default class CharacterCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: false,
+        }
     }
-    }
-   
+
     activate = () => {
-    if(!this.state.active){
-    this.setState({active: true, seq})
+        if (!this.state.active) {
+            this.setState({ active: true})
+        }else this.setState({ active: false})
     }
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
     }
-    render(){
-    let className = `card ${this.state.active ? 'activeCard': ''}`
-    return (
-    <div className={className} onClick={this.activate}>
-    {this.props.value}
-    </div>
-    )
-    }
-   }
+}
